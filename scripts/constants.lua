f2e20317b1bf6b0cd1c5bb308cfe4d1e6c2773d4
Jim Viebke <jviebke@gmail.com>
2024-03-04 19:05:17 -0500

    Formatting

--- scripts/constants.lua
+++ scripts/constants.lua
@@ -1,22 +1,22 @@
 ---@enum SelectorMode
 SelectorMode = {
-  ---sort the input signals in ascending or descending order, then output the signal at the specified index
-  index = "index",
-  ---count the number of input signals, then output the result
-  count_inputs = "count_inputs",
-  ---output a randomly selected signal from among the inputs
-  random_input = "random_input",
-  ---output the stack sizes of the input signals
-  stack_size = "stack_size",
-  ---transfer the quality of an input signal to the output signal(s)
-  quality_transfer = "quality_transfer",
+    ---sort the input signals in ascending or descending order, then output the signal at the specified index
+    index = "index",
+    ---count the number of input signals, then output the result
+    count_inputs = "count_inputs",
+    ---output a randomly selected signal from among the inputs
+    random_input = "random_input",
+    ---output the stack sizes of the input signals
+    stack_size = "stack_size",
+    ---transfer the quality of an input signal to the output signal(s)
+    quality_transfer = "quality_transfer",
 }
 
 Constants = {
-  combinator_name = "selector-combinator",
-  combinator_output_name = "selector-out-combinator",
+    combinator_name = "selector-combinator",
+    combinator_output_name = "selector-out-combinator",
 }
 
 Mods = {
-	janky_quality_name = "janky-quality"
+    janky_quality_name = "janky-quality"
 }
